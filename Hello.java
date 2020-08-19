2252987fd52c8adcd2e21ffbe9d166d14e6a14f3
JosephOsaro <69908230+JosephOsaro@users.noreply.github.com>
2020-08-19 10:14:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**
+*Hello.java
+
+*Joseph Osaro
+*08/17/2020
+*CMSC 255 003
+*/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Joseph Osaro");
+		System.out.println("Hello.java");
+		System.out.println("Hello World Program");
+		System.out.println("08/17/2020");
+		System.out.println("CMSC 255 003");
+		System.out.println();
+		}
+}
